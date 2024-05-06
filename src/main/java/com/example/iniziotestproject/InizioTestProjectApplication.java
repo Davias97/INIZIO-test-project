423c4033b0e442293564596ba1d6ffbd82ba0785
Davias97 <da.vesely@email.cz>
2024-05-06 22:38:01 +0200

    project created

---
+++ src/main/java/com/example/iniziotestproject/InizioTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.iniziotestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InizioTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InizioTestProjectApplication.class, args);
+	}
+
+}
